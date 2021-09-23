fc01b82fa8359adf996c6010e5a2a4792f987ea0
Mohd Hanif Mohamod Rodili <hanifrodili@gmail.com>
2021-09-23 21:39:04 +0800

    add landing page

---
+++ src/components/FeaturesSection.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .features-section
+    div.section-120.bg-tertiary.features
+      v-row
+        v-col.col.pt-5(cols="12" md="5")
+          button.btn.bg-secondary.text-tertiary.btn-2.mt-5.mb-4 Fluid Video Chat
+          .section-heading-text.text-secondary Make virtual interactions more human
+          .section-body-text.text-white
+            | Video chat shouldn&rsquo;t be awkward. Walking in and out of conversations feels natural and seamless in Gather.
+        v-col.col(cols="12" md="7")
+          .media-container-2.ratio-6x4
+            //- img(src='https://via.placeholder.com/600x400?text=Video Anda' width='100%' alt='')
+            video(height="100%" loop autoplay muted)
+              source(src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4" type="video/mp4")
+      v-row.reverse
+        v-col.col.pt-5(cols="12" md="5")
+          button.btn.bg-secondary.text-tertiary.btn-2.mt-5.mb-4 Fluid Video Chat
+          .section-heading-text.text-secondary Make virtual interactions more human
+          .section-body-text.text-white
+            | Video chat shouldn&rsquo;t be awkward. Walking in and out of conversations feels natural and seamless in Gather.
+        v-col.col(cols="12" md="7")
+          .media-container-2.ratio-6x4
+            //- img(src='https://via.placeholder.com/600x400?text=Video Anda' width='100%' alt='')
+            video(height="100%" loop autoplay muted)
+              source(src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4" type="video/mp4")
+      v-row
+        v-col.col.pt-5(cols="12" md="5")
+          button.btn.bg-secondary.text-tertiary.btn-2.mt-5.mb-4 Fluid Video Chat
+          .section-heading-text.text-secondary Make virtual interactions more human
+          .section-body-text.text-white
+            | Video chat shouldn&rsquo;t be awkward. Walking in and out of conversations feels natural and seamless in Gather.
+        v-col.col(cols="12" md="7")
+          .media-container-2.ratio-6x4
+            //- img(src='https://via.placeholder.com/600x400?text=Video Anda' width='100%' alt='')
+            video(height="100%" loop autoplay muted)
+              source(src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4" type="video/mp4")
+</template>
+<script>
+export default {
+  name: "FeaturesSection"
+}
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .gather
+    navbar
+    top-section
+    brand-banner
+    features-section
+    testimoni-section
+    solutions-section
+    cta-banner
+    my-footer
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+import TopSection from '@/components/TopSection.vue'
+import BrandBanner from '@/components/BrandBanner.vue'
+import FeaturesSection from '@/components/FeaturesSection.vue'
+import TestimoniSection from '@/components/TestimoniSection.vue'
+import SolutionsSection from '@/components/SolutionsSection.vue'
+import CtaBanner from '@/components/CtaBanner.vue'
+import MyFooter from '@/components/Footer.vue'
+export default {
+  name: "Gather",
+  components: {
+    Navbar,
+    TopSection,
+    BrandBanner,
+    FeaturesSection,
+    TestimoniSection,
+    SolutionsSection,
+    CtaBanner,
+    MyFooter
+  }
+}
+</script>
+<style scoped>
+ @import '../assets/css/gather.css';
+</style>
